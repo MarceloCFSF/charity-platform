aa326951862cffb409402e7dd68a241764a892b4
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-03-06 19:36:39 -0300

    feat: configure backend

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return "hello world";
+})->middleware('auth:sanctum');
