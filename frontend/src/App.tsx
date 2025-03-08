513edcc7cec9a5adff2748e822e73cb7c564dcc6
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-03-07 22:11:46 -0300

    style: configure style with material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
 
 function App() {
-
   return (
     <BrowserRouter>
       <AppRoutes />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { StrictMode, useMemo } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline, useMediaQuery } from '@mui/material'
+import { darkTheme, lightTheme } from './theme.ts'
+
+export const Root = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () => prefersDarkMode ? darkTheme : lightTheme,
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
